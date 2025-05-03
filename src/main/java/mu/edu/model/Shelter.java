03d63e02ad9f6b0c2cff5d64ca4b7f1bdeb6f895
noahm <noahm@100.110.146.176>
2025-05-02 18:43:41 -0600

    Added Findby Type, Species, Id to Shelther

--- src/main/java/mu/edu/model/Shelter.java
+++ src/main/java/mu/edu/model/Shelter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 
 import mu.edu.pet.Pet;
 
@@ -35,6 +36,29 @@ public class Shelter <T extends Pet> {
 	public boolean removePet(String id) {
 		return animalList.removeIf(pet -> pet.getId().equals(id));
 	}
+	
+	public T findPetById(String id) {
+		for (T pet : animalList) {
+			if (pet.getId().equals(id)) {
+				return pet;
+			}
+		}
+		return null;
+	}
+	
+	public List<T> findPetsByType(String type) {
+		return animalList.stream()
+				.filter(pet -> pet.getType().equalsIgnoreCase(type))
+				.collect(Collectors.toList());
+	}
+	
+	public List<T> findPetsBySpecies(String species) {
+		return animalList.stream()
+				.filter(pet -> pet.getSpecies().equalsIgnoreCase(species))
+				.collect(Collectors.toList());
+	}
+	
+	
 	
 	
 	

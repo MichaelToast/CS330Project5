863a73ce9418aa206c7b515c53b40bf1f50cbc86
MichaelToast <michaelp86788@gmail.com>
2025-05-03 20:06:18 -0500

    Added action listeners to View elements

--- src/main/java/mu/edu/view/AdoptionCenterView.java
+++ src/main/java/mu/edu/view/AdoptionCenterView.java
@@ -9,14 +9,19 @@ import java.awt.event.ActionListener;
 import mu.edu.model.Shelter;
 import mu.edu.pet.Pet;
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent; 
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea; 
 
 public class AdoptionCenterView extends JFrame{
 	// My Version of UserInformationListView.java
 	private JPanel panel;
 	private JList list;
-	private JButton deleteSelectedAnimals;
-	private JButton adoptSelectedAnimals;
+	private JButton deleteSelectedPets;
+	private JButton adoptSelectedPets;
+	private JButton saveButton; 
+	private JTextArea textArea;
+	private JComboBox sortingDropDown;
 	
 	public AdoptionCenterView() {
 		
@@ -31,13 +36,50 @@ public class AdoptionCenterView extends JFrame{
 		list.setBounds(119, 76, 368, 196);
 		panel.add(list);
 		
-		deleteSelectedAnimals = new JButton("Delete Selected Animals");
-		deleteSelectedAnimals.setBounds(121, 304, 161, 36);
-		panel.add(deleteSelectedAnimals);
+		deleteSelectedPets = new JButton("Delete Selected Animals");
+		deleteSelectedPets.setBounds(121, 304, 161, 36);
+		panel.add(deleteSelectedPets);
 		
-		adoptSelectedAnimals = new JButton("Addopt Selected Animals");
-		adoptSelectedAnimals.setBounds(309, 306, 161, 33);
-		panel.add(adoptSelectedAnimals);
+		adoptSelectedPets = new JButton("Addopt Selected Animals");
+		adoptSelectedPets.setBounds(309, 306, 161, 33);
+		panel.add(adoptSelectedPets);
 		
+		
+		/*
+		sortingDropDown = new JComboBox();
+		sortingDropDown.setBounds(119, 43, 56, 22);
+		panel.add(sortingDropDown);
+		
+		textArea = new JTextArea();
+		textArea.setBounds(207, 351, 151, 22);
+		panel.add(textArea);
+		
+		saveButton = new JButton("New button");
+		saveButton.setBounds(398, 43, 89, 23);
+		panel.add(saveButton);
+		*/
 	}
+	
+	public void addActionListenerToDeletePetsButton(ActionListener listener) {
+		deleteSelectedPets.addActionListener(listener);
+	}
+	
+	public void addActionListenerToAdoptPetsButton(ActionListener listener) {
+		deleteSelectedPets.addActionListener(listener);
+	}
+	
+	public DefaultListModel<Pet> getUserList() {
+		return (DefaultListModel<Pet>) list.getModel();
+	}
+	
+	public int getSelectedUser() {
+		System.out.println("Selected Pet index: " + list.getSelectedIndex());
+		return list.getSelectedIndex();
+	}
+	
+	public int[] getMultipleSelectedPets() {
+		return list.getSelectedIndices();
+	}
+	
+	
 }
